5eee479c853f91d248660a7910a483b184a4f4df
Daniel <wlib@users.noreply.github.com>
2016-12-20 17:01:52 -0500

    rename executable to sscrape

--- lib/scrape.rb
+++ lib/scrape.rb
@@ -2,9 +2,16 @@
 # "Scrape" the website and decide what files need to be downloaded
 # Daniel Ethridge
 
+require 'net/http'
 require 'fileutils'
 
 module SurgeScrape
   def scrape(site)
+    uri = URI.parse("#{site}/auto.appcache")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+    puts response.body
   end
 end
